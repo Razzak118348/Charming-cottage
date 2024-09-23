6c535f9abc577ec130bad0b4d05a149ee37e9056
razzak118348 <abdurrazzak118348@gmail.com>
2024-09-23 19:16:59 +0600

    property details finish

--- src/Pages/PropertyDetails/PropertyDetails.jsx
+++ src/Pages/PropertyDetails/PropertyDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router-dom";
-import { FaArrowRight, FaBath, FaBed, FaChartArea } from "react-icons/fa";
-import { FaKitchenSet } from "react-icons/fa6";
+import { FaArrowRight, FaBath, FaBed, FaChartArea, FaPhoneAlt } from "react-icons/fa";
+import { FaKitchenSet, FaLocationDot } from "react-icons/fa6";
 import { GiFlowerPot } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
@@ -23,6 +23,7 @@ const PropertyDetails = () => {
         location,
         facilities,
         image,
+        contacts,
     } = selectedProperty;
 
     return (
@@ -45,7 +46,7 @@ const PropertyDetails = () => {
 
             <h3 className="mx-8  font-medium  text-lg my-3
             ">Facilities of this property :</h3>
-            <hr className="mx-8"/>
+            <hr className="mx-8" />
             <div className="bg-slate-100 p-8 rounded-3xl  mx-8 grid grid-cols-2 lg:grid-cols-3 gap-5 mb-10 mt-3">
                 <div className="flex text-gray-600 ">
                     <FaBed className="mr-2  w-10 h-10"></FaBed>
@@ -79,23 +80,48 @@ const PropertyDetails = () => {
 
             <div className="mb-10">
                 <h3 className="mx-8 text-xl font-bold mb-2">Address :</h3>
-                <hr className="mx-8"/>
-{/* react leaflet  */}
+                <hr className="mx-8" />
+                {/* react leaflet  */}
 
-<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
 
             </div>
+            {/* contact us  */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mb-10">
+                <h3 className="mx-8 text-xl font-bold mb-2">Contact us : </h3>
+                <hr className="mx-8 mb-3" />
 
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title "><FaLocationDot className="w-8 h-8 text-red-700"></FaLocationDot></h2>
+                        <h2 className="card-title">C{contacts[0].address}</h2>
+
+                        <div className="card-actions flex justify-start">
+                            <FaPhoneAlt className="mt-1"></FaPhoneAlt>: {contacts[0].phone}
+                        </div>
+                    </div>
+                </div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <div className="card-body">
+                    <h2 className="card-title"><FaLocationDot className="w-8 h-8 text-red-700"></FaLocationDot></h2>
+                        <h2 className="card-title">C{contacts[1].address}</h2>
+
+                        <div className="card-actions flex justify-start">
+                        <FaPhoneAlt className="mt-1"></FaPhoneAlt>:  {contacts[1].phone}
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
